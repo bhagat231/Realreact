d064f4ffca1dee4c89f965345d0794fd3afc4ca4
bhagat231 <sushantbhagat000@gmail.com>
2022-11-26 00:18:33 +0545

    my third commit reactwebsite

---
+++ src/Comp/Footer.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <div className='container p-3 mb-5 bg-body rounded'>
+        <div className='row'>
+            <div className='col-lg-2 .d-none .d-lg-block .d-xl-none '>
+                <h5>Land for sale</h5>
+                <div className='what'>
+                <p>Land for sale Kathmandu</p>
+                <p> Land for sale Lalitpur</p>  
+                <p> Land for sale Bhaktapur</p>   
+                <p>Land for sale Pokhara</p>   
+                <p> Land for sale Chitwan</p>   
+                <p>Ghar Jagga Nepal</p>  
+                </div>  
+            </div>
+            <div className='col-lg-2 '>
+                <h5>House for sale</h5>
+                <div className='what'>
+                       <p>House for sale Kathmandu</p>         
+                        <p>House for sale Lalitpur</p>         
+                         <p> House for sale Bhaktapur</p>        
+                           <p> House for sale Pokhara</p>        
+                             <p>  House for sale Chitwan</p>       
+                               <p>   Real Estate Nepal</p>       
+                </div>  
+            </div>
+            <div className='col-lg-2 '>
+                <h5>Rentals</h5>
+                <div className='what'>
+                <p>Apartments for rent</p>         
+                <p>Flats for rent</p>         
+                <p> Houses for rent</p>        
+                <p> Offices for rent</p>        
+                <p> Lands for rent</p>       
+                <p>  Sasto Ghar Jagga</p>       
+                </div>  
+            </div>
+            <div className='col-lg-2 '>
+                <h5>Tools</h5>
+                <div className='what'>
+                <p>Area Converter</p>         
+                <p>Type Nepali</p>         
+                <p> Houses for rent</p>  
+                <h5>Advertisement</h5>      
+                <p> Pricing</p>        
+                <p> Instant Listings</p>       
+                <p> Banner Advertisements</p>       
+                </div>  
+            </div>
+            <div className='col-lg-2 '>
+                <h5>Company</h5>
+                <div className='what'>
+                <p>About</p>         
+                <p>Blog & News</p>         
+                <p> Contact</p>  
+                <h5>Carrers</h5>      
+                <p> Testimonials</p>          
+                </div>  
+            </div>
+        </div>
+    </div>
+    <div className='container py-5 '>
+        <div className='row '>
+            <div className='justify-content-center ms-5'>
+                <hr/>
+            <p><i class="bi bi-facebook border-end "></i><span><i class="bi bi-facebook border-end mx-2"></i></span><span><i class="bi bi-facebook border-end mx-2"></i></span><span><i class="bi bi-facebook border-end mx-2"></i></span><span className='text-info border-end mx-2'>Terms of use</span><span className='text-info border-end mx-2'>Privacy Policy</span><span className='text-info border-end mx-2'>Site map</span><span className='text-info border-end mx-2'>About us</span><span className='text-info border-end mx-2'>contact us us</span><span className='text-dark'>Copyright © 2022 Online Real Estate Pvt. Ltd. | All Rights reserved</span></p>
+
+            </div>
+        </div>
+    </div>
+    <div className='container  border-top '>
+        <div className='row lh-1 text-dark text-justify text-center'>
+            <p>Disclaimer:You understand and agree that Gharghaderi.com is NOT involved in transaction of any goods/services listed in the website. It is only a platform for property owners, agents and developers to share information about their property. Gharghaderi.com does not hold any responsibility for their act in any circumstances. Gharghaderi.com is not an agency or agent. It is just a web tool developed to share property related information.</p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+
+
+
+
+
